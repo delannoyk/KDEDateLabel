c4fcca7a1151c1a23165512e893e49fa84ab7da6
Kevin Delannoy <delannoyk@gmail.com>
2016-02-12 19:21:43 +0100

    Added a framework target for Carthage

---
+++ Example/KDEDateLabel/KDEDateLabel.h
@@ -0,0 +1,19 @@
+//
+//  KDEDateLabel.h
+//  KDEDateLabel
+//
+//  Created by Kevin DELANNOY on 12/02/16.
+//  Copyright © 2016 Kevin Delannoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KDEDateLabel.
+FOUNDATION_EXPORT double KDEDateLabelVersionNumber;
+
+//! Project version string for KDEDateLabel.
+FOUNDATION_EXPORT const unsigned char KDEDateLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KDEDateLabel/PublicHeader.h>
+
+
